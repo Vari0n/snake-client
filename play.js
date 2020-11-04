1d7b8797eae13ed5cc01744a9ec01ddc275971dc
Vari0n <Kenly_P@yahoo.ca>
2020-11-04 23:39:44 +0000

    added setupInput  and handleUserInput functions to the file

--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  //console.log('after resume');
+  stdin.on('data', (key) => {
+    //console.log('lock the key' + key);
+    if (key === '\u0003') {
+      process.exit()
+    }
+  })
+  return stdin;
+}
+
+setupInput();
+
