28253fe785da6828313af594ae0df2a249ab73df
Vari0n <Kenly_P@yahoo.ca>
2020-11-04 20:42:54 +0000

    created an event-handler statement of when connection was successful to the game and an even-handler to send data (name for snake) to the server(game)

--- client.js
+++ client.js
@@ -16,6 +16,13 @@ const connect = function() {
   // })
   conn.on('data', (data) =>{
     console.log(data);
+  });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+  //sending data via TCP (giving name to Snake. MAX: 3 letters)
+  conn.on('connect', () => {
+    conn.write('Name: KEN')
   })
 
   return conn;
