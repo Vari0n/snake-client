74a6c874ee8b30d85be8c493f38bb65aa28d15f0
Vari0n <Kenly_P@yahoo.ca>
2020-11-05 02:19:42 +0000

    added two stdin statements to house preset text response to communicate to players in the snake game

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,6 +12,28 @@ const setupInput = function() {
     if (key === '\u0003') {
       process.exit()
     }
+  })
+  stdin.on('data', (key) => {
+    if (key === 'w'){
+      conn.write('Move: up');
+    }
+    if (key === 'a') {
+      conn.write('Move: left');
+    }
+    if (key === 's') {
+      conn.write('Move: down');
+    }
+    if (key === 'd') {
+      conn.write('Move: right');
+    }
+    if (key === 'g') {
+      conn.write('Say: GG');
+    }
+    
+    if (key === 'l') {
+      conn.write('Say: Lol') 
+    }
+
   })
   return stdin;
 }
